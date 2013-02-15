b74a7c0ce21e509365250c14484eb94ba733274f
Darius Kazemi <darius.kazemi@gmail.com>
2013-02-15 17:51:08 -0500

    Updating function return style per r https://gist.github.com/dariusk/4964067

--- rapbot.js
+++ rapbot.js
@@ -6,93 +6,96 @@ var article = require('./lib/indefinite');
 var Wordnik = require('wordnik-bb').init(APIKEY);
 
 var express = require('express'),
-    app = express();
+  app = express();
 app.use(express.logger());
 app.listen(3000);
 console.log('Express server started on port 3000');
 
 function getCoupletPromise() {
 
-var coupletDeferred = _.Deferred();
-var coupletPromise = coupletDeferred.promise();
+  var coupletDeferred = _.Deferred();
+  var coupletPromise = coupletDeferred.promise();
 
-var url = "http://api.wordnik.com//v4/words.json/randomWord?includePartOfSpeech=noun&excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive,suffix,family-name,idiom,affix&minCorpusCount=5000&api_key=" + APIKEY;
-var rwDeferred = _.Deferred();
-var randomWordPromise = rwDeferred.promise();
-request({
-  url : url
-}, function(error, response, body) {
-  if (!error && response.statusCode === 200) {
-    //console.log(JSON.parse(body).word);
-    //console.log(I.singularize(JSON.parse(body).word));
-    var word = new Wordnik.Word({word: I.singularize(JSON.parse(body).word), params:{
-        relationshipTypes: 'rhyme',
-        limitPerRelationshipType: 100,
-        hasDictionaryDef: true
-      }});
-    word.getWord()
-     .then( function() {
+  var url = "http://api.wordnik.com//v4/words.json/randomWord?includePartOfSpeech=noun&excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive,suffix,family-name,idiom,affix&minCorpusCount=5000&api_key=" + APIKEY;
+  var rwDeferred = _.Deferred();
+  var randomWordPromise = rwDeferred.promise();
+  request({
+    url: url
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      //console.log(JSON.parse(body).word);
+      //console.log(I.singularize(JSON.parse(body).word));
+      var word = new Wordnik.Word({
+        word: I.singularize(JSON.parse(body).word),
+        params: {
+          relationshipTypes: 'rhyme',
+          limitPerRelationshipType: 100,
+          hasDictionaryDef: true
+        }
+      });
+      word.getWord()
+        .then(function () {
         rwDeferred.resolve(word);
       });
 
-  }
-  else {
-    rwDeferred.reject(error);
-  }
-});
+    }
+    else {
+      rwDeferred.reject(error);
+    }
+  });
 
-randomWordPromise.done(function(word) {
-  //console.log("The model for our random word: ", word);
-  // We could also get more info about the random word, in this case, relatedWords that rhyme:
-  word.getRelatedWords()
-    .then( function() {
+  randomWordPromise.done(function (word) {
+    //console.log("The model for our random word: ", word);
+    // We could also get more info about the random word, in this case, relatedWords that rhyme:
+    word.getRelatedWords()
+      .then(function () {
       if (word.get("relatedWords").length > 0) {
-      var first = getLine(word.id,'noun');
-      var word2 = word.get("relatedWords")[0].words[Math.floor(Math.random()*word.get("relatedWords")[0].words.length)];
-      var posPromise = getPartOfSpeech(word2);
-      ( function(first, word2) {
-      
-      posPromise.done(function(pos) {
-        var result = "oops!";
-        result = getLine(word2, pos);
-        if (result === "") {
-          coupletDeferred.resolve(result);
-        }
-        else {
-          coupletDeferred.resolve(first + "\n<br>" + result + "\n<br>");
-        }
-       });
-      })(first, word2);
+        var first = getLine(word.id, 'noun');
+        var word2 = word.get("relatedWords")[0].words[Math.floor(Math.random() * word.get("relatedWords")[0].words.length)];
+        var posPromise = getPartOfSpeech(word2);
+        (function (first, word2) {
+
+          posPromise.done(function (pos) {
+            var result = "oops!";
+            result = getLine(word2, pos);
+            if (result === "") {
+              coupletDeferred.resolve(result);
+            }
+            else {
+              coupletDeferred.resolve(first + "\n<br>" + result + "\n<br>");
+            }
+          });
+        })(first, word2);
       }
       else {
         //coupletDeferred.resolve("Sorry. We couldn't find anything that rhymes with " + word.id + "!");
         coupletDeferred.resolve("");
       }
     });
-});
-return coupletDeferred.promise();
+  });
+  return coupletDeferred.promise();
 }
 
-app.get('/' , function(req, res){
+app.get('/', function (req, res) {
 
   var cypher = "";
 
   var stuffToDo = [];
-  for (var i=0;i<12;i++) {
+  for (var i = 0; i < 12; i++) {
     var cp = getCoupletPromise();
-    cp.done(function(couplet) {
-      if(couplet !== "") {
+    cp.done(function (couplet) {
+      if (couplet !== "") {
         cypher += (couplet);
       }
     });
     stuffToDo.push(cp);
   }
 
-  _.when(stuffToDo).done( function() {
+  _.when(stuffToDo).done(function () {
     //console.log(cypher);
     //console.log('*drops the mic*');
     cypher += "<br>*drops the mic*";
-    res.send('<!doctype html><html><head><title>Freestyle 80s Battle Rap Generator</title><style type="text/css">a {color: rgb(35, 40, 104); text-decoration:none;}</style></head><body style="font-family:sans-serif;width:600px;"><h1>Freestyle 80s Battle Rap Generator</h1><p>'+cypher+'</p><script type="text/javascript"> var _gaq = _gaq || []; _gaq.push(["_setAccount", "UA-37844294-1"]); _gaq.push(["_trackPageview"]); (function() { var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true; ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s); })(); </script></body></html>');
+    res.send('<!doctype html><html><head><title>Freestyle 80s Battle Rap Generator</title><style type="text/css">a {color: rgb(35, 40, 104); text-decoration:none;}</style></head><body style="font-family:sans-serif;width:600px;"><h1>Freestyle 80s Battle Rap Generator</h1><p>' + cypher + '</p><script type="text/javascript"> var _gaq = _gaq || []; _gaq.push(["_setAccount", "UA-37844294-1"]); _gaq.push(["_trackPageview"]); (function() { var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true; ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js"; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s); })(); </script></body></html>');
 
   });
 
@@ -101,32 +104,39 @@ app.get('/' , function(req, res){
 
 function getPartOfSpeech(wordId) {
   // accepts a word string
-  var word = new Wordnik.Word({word: wordId, params:{includeSuggestions:true}});
+  var word = new Wordnik.Word({
+    word: wordId,
+    params: {
+      includeSuggestions: true
+    }
+  });
   var deferred = _.Deferred();
   word.getDefinitions()
-   .then( function(word) {
-      deferred.resolve(word.get("definitions")[0].partOfSpeech);
-    });
+    .then(function (word) {
+    deferred.resolve(word.get("definitions")[0].partOfSpeech);
+  });
   return deferred.promise();
 }
 
 function ladiesFellas() {
-  return (Math.random()<0.5) ? "ladies" : "fellas";
+  return (Math.random() < 0.5) ? "ladies" : "fellas";
 }
 
 function womanMan() {
-  return (Math.random()<0.5) ? "woman" : "man";
+  return (Math.random() < 0.5) ? "woman" : "man";
 }
 
 function sistasHomies() {
-  return (Math.random()<0.5) ? "sistas" : "homies";
+  return (Math.random() < 0.5) ? "sistas" : "homies";
 }
 
 function w(word) {
-  return "<a href='http://www.wordnik.com/words/"+word+"'>"+word+"</a>";
+  return "<a href='http://www.wordnik.com/words/" + word + "'>" + word + "</a>";
 }
 
 function getLine(word, pos) {
+
+  var result = "Oops, we didn't account for something.";
 
   if (pos === 'adjective') {
     var pre = [
@@ -134,9 +144,8 @@ function getLine(word, pos) {
       "I make the MCs in the place wish that they were ",
       "My rhymes blow your mind and you think it's ",
       "My sweet-ass rhymes make your " + womanMan() + " feel ",
-      "Now I'm gonna tell you why you ain't ",
-    ];
-    return(pre[Math.floor(Math.random()*pre.length)] + w(word));
+      "Now I'm gonna tell you why you ain't ", ];
+    result = pre[Math.floor(Math.random() * pre.length)] + w(word);
   }
   else if (pos === 'noun' || pos === 'proper-noun') {
     var a = article(word) + " ";
@@ -155,22 +164,22 @@ function getLine(word, pos) {
       "My posse's got my back and my " + sistasHomies() + " got my ",
       "Sweeter than molasses, and stronger than " + a,
       "Try to step to me and I'mma wreck your ",
-      "Wherever I go, people give me some "
-    ];
-    return(pre[Math.floor(Math.random()*pre.length)] + w(I.singularize(word)));
+      "Wherever I go, people give me some "];
+    result = pre[Math.floor(Math.random() * pre.length)] + w(I.singularize(word));
   }
   else if (pos === 'verb-transitive') {
-    return("My rhyme profile makes the " + ladiesFellas() + " " + w(word));
+    result = "My rhyme profile makes the " + ladiesFellas() + " " + w(word);
   }
   else if (pos === 'interjection') {
-    return("*skratch solo* ... (" + word[0] + "-" + word[0] + "-" + w(word) + "!)");
+    result = "*skratch solo* ... (" + word[0] + "-" + word[0] + "-" + w(word) + "!)";
   }
   else {
     //result = pos;
     //coupletDeferred.resolve(result);
     //coupletDeferred.resolve("");
-    return("");
+    result = "";
   }
 
-}
+  return result;
 
+}
